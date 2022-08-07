628b7d55674bcbdea7f506168ab5206634ce8da5
Brian <briansuarezsantiago@gmail.com>
2022-08-07 15:00:11 +0100

    Kata 2. Version 1

---
+++ src/histogram/Main.java
@@ -0,0 +1,36 @@
+package histogram;
+
+import java.util.Map;
+import java.util.HashMap;
+/**
+ * Class implementing a histogram. Starting from an array
+ * with integer values, an application of console has been
+ * built that returns the frequency/number of occurrences
+ * of each value in the array.
+ *
+ * @author Brian Suárez Santiago
+ * @version 1.0.0
+ * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/Map.html">Interface Map</a>
+ * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.html">Class HashMap</a>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int[] data = {0, 7, 4, 3, 0, 5, 7, 2, 1, 4, 9, 0, 6, 5, 2, 7, 8, 7, 4, 7};    // Represents the keys
+        Map<Integer, Integer> histogram = new HashMap<>();
+
+        for(int i = 0; i < data.length; i++) {
+            if(histogram.containsKey(data[i])) {
+                histogram.put(data[i], histogram.get(data[i]) + 1);
+            } else {
+                histogram.put(data[i], 1);
+            }
+        }
+
+        if(!histogram.isEmpty()) {
+            for(Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
+                System.out.println(entry.getKey() + " --> " + entry.getValue());
+            }
+        }
+    }
+}
